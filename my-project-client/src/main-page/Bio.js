abf7deb49333ced3081c28b5a2ecf8d8d8ae2f88
Michael Williamson <michaelrwilliamson92@gmail.com>
2020-03-10 12:21:14 +0000

    will add to navbar

--- my-project-client/src/main-page/Bio.js
+++ my-project-client/src/main-page/Bio.js
@@ -8,6 +8,8 @@ export default class Bio extends React.Component {
                     <h1> adding for reference to note that bio should probably be added into app before plans to host anywhere. ? </h1>
                     <h2> maybe add in picture of self? </h2>
 
+                    <h3> ave navbar include buttons to link here </h3>
+
                 </div>
         )
     }
