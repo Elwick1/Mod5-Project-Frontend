857350ea1df352d3b4c89202fd89d7395df4f9ef
Michael Williamson <michaelrwilliamson92@gmail.com>
2020-03-10 12:19:50 +0000

    thinking about re adding in bio page

--- my-project-client/src/main-page/Bio.js
+++ my-project-client/src/main-page/Bio.js
@@ -5,7 +5,7 @@ export default class Bio extends React.Component {
     render() {
         return(
                 <div>
-                    
+                    <h1> adding for reference to note that bio should probably be added into app before plans to host anywhere. ? </h1>
                 </div>
         )
     }
