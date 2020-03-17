6cd6dec84869525aa57908f15bb66fc0fdc672b6
Michael Williamson <michaelrwilliamson92@gmail.com>
2020-03-17 21:52:13 +0000

    new component

---
+++ my-project-client/src/main-page/new branches.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Bio from './Bio'
+import '../App.css'
+
+
+export default class Newbranches extends React.Component {
+
+render() {
+    return(
+        <div>
+            <h1> here i will imoplement</h1>
+            <h2> an actual graph or chart of the choics that each playthrough has made"
+
+            </h2>
+            <h3> perhaps a form t create users?</h3>
+            <h4> will definitly add form"
+                
+            </h4>
+        </div>
+    )
